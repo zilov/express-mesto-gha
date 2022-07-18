95b93c7dd9608f760b8ccc31bc3590a72ed71c42
Danil Zilov <zilov.d@gmail.com>
2022-07-18 18:03:13 +0300

    error handler on update user

--- controllers/users.js
+++ controllers/users.js
@@ -40,12 +40,7 @@ const updateUser = (req, res) => {
     },
     (err, user) => {
       if (!user) {
-        return res.status(404).send({ message : err.message })
-      }
-      if (err) {
-        if (err.name === 'ValidationError') {
-          return res.status(400).send({ message : err.message })
-        }
+        return res.status(400).send({ message : err.message })
       }
       return res.send(user)
     })
